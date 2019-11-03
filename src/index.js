da25c44ca8d4bbc99ef072a4aaa4355de475ae85
Zanetti <znti.rafael@gmail.com>
2019-11-02 22:03:37 -0300

    Add signIn and database listener

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
-module.exports = () => console.log('configs are', configs, 'and credentials', creds);
+firebase.initializeApp(configs);
+
+const db = firebase.firestore();
+
+const { email, password } = creds;
+firebase.auth().signInWithEmailAndPassword(email, password)
+	.then((user) => {
+		console.log('Authenticated successfully.', user);
+		setListeners();
+	})
+
+function setListeners() {
+	db.collection("test")
+	.onSnapshot(function(snapshot) {
+		snapshot.docChanges().forEach(function(change) {
+			if (change.type === "added") {
+				console.log("New: ", change.doc.data());
+			}
+			if (change.type === "modified") {
+				console.log("Modified: ", change.doc.data());
+			}
+			if (change.type === "removed") {
+				console.log("Removed: ", change.doc.data());
+			}
+		});
+	});
+}
+//module.exports = () => console.log('configs are', configs, 'and credentials', creds);
 
 
